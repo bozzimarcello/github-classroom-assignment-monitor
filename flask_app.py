d4b2f69c3a9ffb832d74f6c75adf0fc80b178e48
Marcello Bozzi <bozzimarcello@gmail.com>
2023-03-13 10:02:45 +0100

    aggiunta connessione a github (TESTARE)

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from elaborazione_csv import ElaboraCSV
+from github import Github
 
 app = Flask(__name__)
 
@@ -16,7 +17,15 @@ def dati():
     lista_dati = elabora.analizza_dati(filename)
     return render_template('dati.html', lista_dati=lista_dati)
 
+@app.route('/accesso', methods=["GET", "POST"])
+def accesso():
+    if request.method == "POST":
+        token = request.form["token"]
+        g = Github(token)
+    return render_template('accesso.html')
+
 @app.route('/compiti')
 def compiti():
     lista_compiti = []
     return render_template('compiti.html', lista_compiti=lista_compiti)
+
