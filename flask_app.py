85e71a2d8b173eb5acfd2d505716849899bd8e60
Marcello Bozzi <bozzimarcello@gmail.com>
2023-03-15 10:43:42 +0100

    l'accesso è a posto

--- flask_app.py
+++ flask_app.py
@@ -19,14 +19,19 @@ def dati():
 
 @app.route('/accesso', methods=["GET", "POST"])
 def accesso():
+    utente = "--NON CONNESSO--"
     if request.method == "POST":
-        token = request.form["token"]
+        token = request.form["mio-token"]
         g = Github(token)
-        return redirect(url_for('index'))
-    return render_template('accesso.html')
+        utenteObj = g.get_user()
+        utente=utenteObj.login
+        #return redirect(url_for('index'))
+    return render_template('accesso.html', utente=utente)
 
 @app.route('/compiti')
 def compiti():
     lista_compiti = []
     return render_template('compiti.html', lista_compiti=lista_compiti)
 
+if __name__ == '__main__':
+    app.run()
